d4aebb1d7fbba25667da00c8838d38852a1f53a9
kleberboy <kleberboy@erebos.xyz>
2019-02-13 02:57:33 +0100

    add gitup script (backup solution)

---
+++ gitup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -e /etc/systemd/system/gitup.timer ]; then
+
+ln $DIR/systemd/gitup.* /etc/systemd/system/
+systemctl daemon-reload
+systemctl start gitup.timer
+systemctl enable gitup.timer
+
+fi
+
+systemctl stop skytekx.service
+sleep 39
+cd $DIR
+git add .
+git commit -m "$(date) automated backup script (gitup.sh)"
+git push -uf origin master;
+reboot
+
+exit 0
